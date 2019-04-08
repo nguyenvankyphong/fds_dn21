47bda40a0a1498281ad2de239f7f25686da07a62
Phong Ky <nguyenvankyphong@gmail.com>
2019-04-08 14:28:16 +0700

    add item to cart and remove item form cart

--- app/controllers/client/clients_controller.rb
+++ app/controllers/client/clients_controller.rb
@@ -1,3 +1,5 @@
 class Client::ClientsController < ApplicationController
   def index; end
+
+  include Client::CartsHelper
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,36 @@
 Category.create!(name: "All products", parent_id: 0)
 
-10.times do |n|
-  name = "Category-#{n+1}"
-  parent_id = rand(0..n-1)
-  picture = "chua-chuc-thanh-7"
+2.times do |n|
+  category_names = ["Food", "Drink"]
+  name = category_names[n]
+  parent_id = 1
   Category.create!(name: name, parent_id: parent_id)
+end
+
+2.times do |n|
+  category_names = ["Coffee", "Tea"]
+  name = category_names[n]
+  parent_id = 3
+  Category.create!(name: name, parent_id: parent_id)
+end
+
+4.times do |n|
+  product_names = ["Black Coffee", "Espresso", "Green Tea", "Black Tea"]
+  category_ids = [4, 4, 5, 5]
+  price_values = [30000, 40000, 50000, 60000]
+  Product.create!(name: product_names[n],
+    price: price_values[n],
+    quantity: 10,
+    category_id: category_ids[n])
 end
 
 User.create!(name: "Nguyen Chanh Huan", email: "admin@gmail.com", address: "DN",
   phone: "0123456789", password: "111111", password_confirmation: "111111", role: 1)
 
 User.create!(name: "client", email: "client@gmail.com", address: "DN",
+  phone: "0123456789", password: "111111", password_confirmation: "111111", role: 0)
+
+User.create!(name: "Ky Phong", email: "phong@gmail.com", address: "DN",
   phone: "0123456789", password: "111111", password_confirmation: "111111", role: 0)
 
 5.times do |n|
@@ -21,4 +41,25 @@ User.create!(name: "client", email: "client@gmail.com", address: "DN",
   password = "111111"
   User.create!(name: name, email: email, address: address, phone: phone, password: password,
     password_confirmation: password)
+end
+
+2.times do |n|
+  users = [2, 3]
+  status = 3
+  Order.create!(
+    user_id: users[n],
+    status: 3)
+end
+
+4.times do |n|
+  OrderProduct.create!(
+    product_id: n+1,
+    price: Product.find(n+1).price,
+    quantity: 2,
+    order_id: 1)
+  OrderProduct.create!(
+    product_id: n+1,
+    price: Product.find(n+1).price,
+    quantity: 2,
+    order_id: 2)
 end
