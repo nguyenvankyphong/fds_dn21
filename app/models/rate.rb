dde1a843e0f1607d206d3e5e9cdd59e0a0b9a0ea
Phong Ky <nguyenvankyphong@gmail.com>
2019-04-15 15:19:49 +0700

    rate and comment for each product

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,8 @@
 class Rate < ApplicationRecord
+  validates :product_id, presence: true
+
   belongs_to :user
   belongs_to :product
+
+  scope :order_by_created_time, ->{order created_at: :desc}
 end
